e0b94ea26b3774f95f6ef4b8c4c7f053ec0f1ba2
codecake <codec4ke@gmail.com>
2012-01-23 15:16:13 -0500

    runner

--- src/org/ranx/Runner.java
+++ src/org/ranx/Runner.java
@@ -23,17 +23,39 @@ import org.antlr.runtime.*;
 import org.antlr.runtime.tree.*;
 
 public class Runner {
+	private static void runLine(String line) {
+		try {
+	        RanxLexer lex = new RanxLexer(new ANTLRStringStream(line));
+	        CommonTokenStream tokens = new CommonTokenStream(lex);
+	 
+	        RanxParser parser = new RanxParser(tokens); // created from T.g
+	        RanxParser.formula_return r = parser.formula(); // launch parsing
+	        if ( r!=null ) System.out.println(((CommonTree)r.getTree()).toStringTree());
+	 
+	        CommonTreeNodeStream nodes = new CommonTreeNodeStream((Tree)r.getTree());
+	        nodes.setTokenStream(tokens);
+	        Eval walker = new Eval(nodes); // created from TP.g
+	        walker.prog();
+		} catch (RecognitionException ex) {
+			System.err.println(ex.toString());
+		}
+	}
+	
+	public static void printHeader() {
+		System.out.print("ranx v0.1");
+	}
+	
     public static void main(String args[]) throws Exception {
-        RanxLexer lex = new RanxLexer(new ANTLRFileStream(args[0]));
-        CommonTokenStream tokens = new CommonTokenStream(lex);
- 
-        RanxParser parser = new RanxParser(tokens); // created from T.g
-        RanxParser.formula_return r = parser.formula(); // launch parsing
-        if ( r!=null ) System.out.println(((CommonTree)r.getTree()).toStringTree());
- 
-        CommonTreeNodeStream nodes = new CommonTreeNodeStream((Tree)r.getTree());
-        nodes.setTokenStream(tokens);
-        Eval walker = new Eval(nodes); // created from TP.g
-        walker.prog();
+    	InputStreamReader converter = new InputStreamReader(System.in);
+    	BufferedReader reader = new BufferedReader(converter);
+    	while(true) {
+    		printHeader();
+    		System.out.print("ranx> ");
+    		String line = reader.readLine();
+    		if(line.equals("quit")) {
+    			break;
+    		}
+    		runLine(line);
+    	}    	
     }
 }
